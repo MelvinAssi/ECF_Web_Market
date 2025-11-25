147eccf50dbea6a5fba3cc866a555152c28333ea
MelvinAssi <melvinassi.pro@gmail.com>
2025-11-25 21:20:51 +0100

    render key

--- my-backend-express/config/firebase.js
+++ my-backend-express/config/firebase.js
@@ -1,11 +1,18 @@
 const admin = require("firebase-admin");
+const fs = require("fs");
 
-const serviceAccount = require("./serviceAccountKey.json");
+
+const secretFilePath = "/etc/secrets/serviceAccountKey.json";
+let serviceAccount;
+
+if (fs.existsSync(secretFilePath)) {
+  serviceAccount = require(secretFilePath);
+} else {
+  serviceAccount = require("./serviceAccountKey.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-const db = admin.firestore();
-
-module.exports = db;
+module.exports = admin.firestore();
