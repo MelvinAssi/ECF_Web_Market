8613bff1552abf494a5f7448fd5a6561374c37b3
MelvinAssi <melvinassi.pro@gmail.com>
2025-11-25 23:32:44 +0100

    update render start server

--- my-backend-express/config/db.js
+++ my-backend-express/config/db.js
@@ -2,13 +2,25 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres',
-  logging: false, 
-});
+const isRender = process.env.RENDER === "true";
 
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.DB_PORT || 5432,
+    logging: false,
+    dialectOptions: isRender
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+  }
+);
 module.exports = sequelize;
-
-
